5e1236e587e3c194d33a590ea958db77e0d0c751
Michiel Dethmers <git@phplist.com>
2017-05-21 21:48:53 +0100

    use same start and stop script for normal and dev versions

--- stop-phplist.sh
+++ stop-phplist.sh
@@ -3,15 +3,10 @@
 FILE=phplist.yml
 cd $(dirname $0)
 
-## check if any of the DEV settings are found
 . .env
 if [ -n "$THEME_DEV_PATH" ] || [ -n "$CODE_PATH" ] || [ -n "$CODE_PATH" ]; then
     FILE=phplist-dev.yml
     echo Using the DEV version
 fi
 
-
-docker-compose -f $FILE down --remove-orphans
-docker-compose -f $FILE pull
-docker-compose -f $FILE up -d
-
+docker-compose -f $FILE down
