cfc15d9d893b8a66286c29e5167e1e7a3344bbe6
Michiel Dethmers <git@phplist.com>
2017-05-22 21:08:03 +0100

    require all dev paths to be set

--- start-phplist.sh
+++ start-phplist.sh
@@ -5,11 +5,10 @@ cd $(dirname $0)
 
 ## check if any of the DEV settings are found
 . .env
-if [ -n "$THEME_DEV_PATH" ] || [ -n "$CODE_PATH" ] || [ -n "$CODE_PATH" ]; then
+if [ -n "$THEME_DEV_PATH" ] && [ -n "$CODE_PATH" ] && [ -n "$CODE_PATH" ]; then
     FILE=phplist-dev.yml
     echo Using the DEV version
 fi
-
 
 docker-compose -f $FILE down --remove-orphans
 docker-compose -f $FILE pull
