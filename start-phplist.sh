71873f1fc49b344a7c2ead204430106ce136212f
Michiel Dethmers <git@phplist.com>
2018-11-16 00:31:53 +0000

    update

--- start-phplist.sh
+++ start-phplist.sh
@@ -2,15 +2,17 @@
 
 FILE=phplist.yml
 cd $(dirname $0)
+test=$1
 
 ## check if any of the DEV settings are found
 . .env
-if [ -n "$THEME_DEV_PATH" ] && [ -n "$CODE_PATH" ] && [ -n "$CODE_PATH" ]; then
+if [ -z $test ] && [ -n "$THEME_DEV_PATH" ] && [ -n "$CODE_PATH" ] && [ -n "$CODE_PATH" ]; then
     FILE=phplist-dev.yml
     echo Using the DEV version
 fi
 
-docker-compose -f $FILE down --remove-orphans
+docker stop $(docker ps -q)
+docker-compose -f $FILE down --remove-orphans 
 docker-compose -f $FILE pull
-docker-compose -f $FILE up -d
+docker-compose -f $FILE up -d --build -V
 
