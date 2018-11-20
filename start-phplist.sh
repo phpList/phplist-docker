bee5778e60a8d86ea6503c59fc7416d540ea71bd
Alex Turbov <i.zaufi@gmail.com>
2018-11-20 13:13:07 +0300

    Improve `start-phplist.sh` a little
    - use bash's builtin `[[` instead of `[`;
    - remove unused/useless positional parameter;
    - fix copy-n-paste typo on developer's execution check;
    - make `docker stop` conditionally to prevent "surprising"
      users who unfamiliar of this "feature".

--- start-phplist.sh
+++ start-phplist.sh
@@ -2,17 +2,18 @@
 
 FILE=phplist.yml
 cd $(dirname $0)
-test=$1
 
 ## check if any of the DEV settings are found
 . .env
-if [ -z $test ] && [ -n "$THEME_DEV_PATH" ] && [ -n "$CODE_PATH" ] && [ -n "$CODE_PATH" ]; then
+if [[ -n $THEME_DEV_PATH ]] && [[ -n $CODE_PATH ]] && [[ -n $PLUGIN_DEV_PATH ]]; then
     FILE=phplist-dev.yml
     echo Using the DEV version
 fi
 
-docker stop $(docker ps -q)
+if [[ $FORCE_STOP_ALL_BEFORE =~ (y(es)?|true|1) ]]; then
+    docker stop $(docker ps -q)
+fi
+
 docker-compose -f $FILE down --remove-orphans 
 docker-compose -f $FILE pull
 docker-compose -f $FILE up -d --build -V
-
