1328ed97b62f7cc72f11bf18d787bd58745f86c6
Alex Turbov <i.zaufi@gmail.com>
2018-11-20 13:32:21 +0300

    Allow to override default compose file
    One can pass a file name to the start/stop scripts
    to override default `phplist.yml` compose file.

--- start-phplist.sh
+++ start-phplist.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-FILE=phplist.yml
 cd $(dirname $0)
 
-## check if any of the DEV settings are found
+FILE=${1:-phplist.yml}
+
+# Sourcing the configuration file.
+# NOTE One can override default compose file here
 . .env
+
+# Check if any of the DEV settings are found
 if [[ -n $THEME_DEV_PATH ]] && [[ -n $CODE_PATH ]] && [[ -n $PLUGIN_DEV_PATH ]]; then
     FILE=phplist-dev.yml
     echo Using the DEV version
@@ -14,6 +18,6 @@ if [[ $FORCE_STOP_ALL_BEFORE =~ (y(es)?|true|1) ]]; then
     docker stop $(docker ps -q)
 fi
 
-docker-compose -f $FILE down --remove-orphans 
+docker-compose -f $FILE down --remove-orphans
 docker-compose -f $FILE pull
 docker-compose -f $FILE up -d --build -V
--- stop-phplist.sh
+++ stop-phplist.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-FILE=phplist.yml
 cd $(dirname $0)
 
+FILE=${1:-phplist.yml}
+
+# Sourcing the configuration file.
+# NOTE One can override default compose file here
 . .env
-if [ -n "$THEME_DEV_PATH" ] || [ -n "$CODE_PATH" ] || [ -n "$CODE_PATH" ]; then
+
+if [[ -n $THEME_DEV_PATH ]] && [[ -n $CODE_PATH ]] && [[ -n $PLUGIN_DEV_PATH ]]; then
     FILE=phplist-dev.yml
     echo Using the DEV version
 fi
