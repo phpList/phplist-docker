ff7804dc50e398b5e8659ea772fc40e81ceb64dc
Michiel Dethmers <michiel@phplist.com>
2022-01-23 13:02:06 +0000

    update file reference

--- data/phplist-entrypoint.sh
+++ data/phplist-entrypoint.sh
@@ -6,6 +6,6 @@ mysqladmin -uroot -p${MYSQL_ROOT_PASSWORD} create phplistdb
 mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "grant all on phplistdb.* to phplist@'%' identified by 'phplist'"
 
 gunzip -c /data.sql.gz | mysql -uphplist -pphplist phplistdb
-rm -f /data.sql.gz
+#rm -f /data.sql.gz
 
 
